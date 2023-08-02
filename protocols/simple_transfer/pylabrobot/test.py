2c5a724da4df7777e0d129dd85f5d2f236e1e2c4
LuHesketh <luuhgomes98@gmail.com>
2023-08-02 14:48:17 -0300

    updated Pylabrobot test protocol

--- protocols/simple_transfer/pylabrobot/test.py
+++ protocols/simple_transfer/pylabrobot/test.py
@@ -2,26 +2,36 @@
 import asyncio
 
 from pylabrobot.liquid_handling import LiquidHandler, STAR
-from pylabrobot.resources import Cos_96_EZWash, HTF_L, Coordinate, STARLetDeck
+from pylabrobot.resources import Cos_96_EZWash, Cos_96_PCR, HTF_L, Coordinate, STARLetDeck
+
+backend = STAR()
+lh = LiquidHandler(backend=backend, deck=STARLetDeck())
+deck=STARLetDeck()
 
 async def __init__():
-  backend = STAR()
-  lh = LiquidHandler(backend=backend, deck=STARLetDeck())
   await lh.setup()
-  tip_rack = HTF_L(name="tip rack")
 
+#assign the tiprack
+tip_rack = HTF_L(name="tip rack")
 lh.deck.assign_child_resource(tip_rack, location=Coordinate(x=100, y=100, z=100))
-plate = Cos_96_EZWash(name="plate")
-lh.deck.assign_child_resource(plate, location=Coordinate(x=200, y=100, z=100))
 
+#assign the PCR plate
+PLASMID_plate = Cos_96_EZWash(name="PLASMID_plate")
+lh.deck.assign_child_resource(PLASMID_plate, location=Coordinate(x=200, y=100, z=100))
+#assign the MPE plate
+MPE_plate = Cos_96_EZWash(name="MPE_plate")
+lh.deck.assign_child_resource(MPE_plate, location=Coordinate(x=400, y=100, z=100))
+
+
+#execute the liquid transfering
 async def liquid_handling_sequence():
-    await lh.pick_up_tips(tip_rack["A1:A2"])
-    await lh.aspirate(plate["A1:A2"], vols=100, flow_rates=100, end_delay=0.5,
+  await lh.pick_up_tips(tip_rack["A1:A2:A3"])
+  await lh.aspirate(PLASMID_plate["A1:A2:A3"], vols=20, flow_rates=100, end_delay=0.5,
 offsets=Coordinate(1, 2, 3))
-    await lh.dispense(plate["A1:A2"], vols=100)
-    await lh.return_tips()
+  await lh.dispense(MPE_plate["A1:A2:A3"], vols=20)
+  await lh.return_tips()
 
-    await asyncio.gather(liquid_handling_sequence(),backend.refill_pump())
+  await asyncio.gather(liquid_handling_sequence(),backend.refill_pump())
 
 
 
